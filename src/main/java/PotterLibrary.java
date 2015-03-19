50c8966d60ca4416f5d19ffab172b5889c1a1785
alvaro garcia <derecerca@gmail.com>
2015-03-19 21:14:05 +0100

    refactor: extract constant - book_price

--- src/main/java/PotterLibrary.java
+++ src/main/java/PotterLibrary.java
@@ -3,12 +3,14 @@
  */
 public class PotterLibrary {
 
+	public static final int BOOK_PRICE = 8;
+
 	public double price (final int... book) {
 		if(book.length == 2){
-			return 8*2*(1-0.05);
+			return BOOK_PRICE *2*(1-0.05);
 		} else if (book.length == 3){
-			return 8*3*(1-0.1);
+			return BOOK_PRICE*3*(1-0.1);
 		}
-		return 8 * book[0];
+		return BOOK_PRICE * book[0];
 	}
 }
