c67861d081716b38d1ccf53526fab0a0be8799ba
alvaro garcia <derecerca@gmail.com>
2015-03-19 21:17:00 +0100

    refactor: replace algorithm - price

--- src/main/java/PotterLibrary.java
+++ src/main/java/PotterLibrary.java
@@ -6,11 +6,14 @@ public class PotterLibrary {
 	public static final int BOOK_PRICE = 8;
 
 	public double price (final int... book) {
-		if(book.length == 2){
-			return BOOK_PRICE *book.length*(1-0.05);
-		} else if (book.length == 3){
-			return BOOK_PRICE*book.length*(1-0.1);
+		switch (book.length) {
+			default:
+			case 1:
+				return BOOK_PRICE * book[0];
+			case 2:
+				return BOOK_PRICE * book.length * (1 - 0.05);
+			case 3:
+				return BOOK_PRICE * book.length * (1 - 0.1);
 		}
-		return BOOK_PRICE * book[0];
 	}
 }
