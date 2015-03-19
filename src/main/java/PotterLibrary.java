e7657f6f083cc3e2cbd29e4d086200a955bd4ea8
alvaro garcia <derecerca@gmail.com>
2015-03-19 21:10:49 +0100

    green: sell_three_different_books_at_10percent_discount

--- src/main/java/PotterLibrary.java
+++ src/main/java/PotterLibrary.java
@@ -6,6 +6,8 @@ public class PotterLibrary {
 	public double price (final int... book) {
 		if(book.length == 2){
 			return 15.2d;
+		} else if (book.length == 3){
+			return 8*3*(1-0.1);
 		}
 		return 8 * book[0];
 	}
--- src/test/java/PotterLibraryShould.java
+++ src/test/java/PotterLibraryShould.java
@@ -20,13 +20,18 @@ public class PotterLibraryShould {
 
 	@Test
 	public void sell_two_different_books_at_5percent_discount () {
-		assertThat(priceFor(1,2), is(percent_discount(5)));
+		assertThat(priceFor(1,2), is(percent_discount(5,2)));
 	}
 
-	private double percent_discount (final int percent) {
+	@Test
+	public void sell_three_different_books_at_10percent_discount () {
+		assertThat(priceFor(1,2,3), is(percent_discount(10,3)));
+	}
+
+	private double percent_discount (final int percent, final int books) {
 		final double totalOfOne = percent / 100d;
 		final double priceOfOne = 1 - totalOfOne;
-		return (8+8)*(priceOfOne);
+		return (8*books)*(priceOfOne);
 	}
 
 	private double priceFor (final int... book) {
