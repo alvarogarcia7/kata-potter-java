d1fba717da0e0d5ca28bd1afa1b344b07ef4b19f
alvaro garcia <derecerca@gmail.com>
2015-03-19 21:13:33 +0100

    refactor: replace algorithm for 2 different books

--- src/main/java/PotterLibrary.java
+++ src/main/java/PotterLibrary.java
@@ -5,7 +5,7 @@ public class PotterLibrary {
 
 	public double price (final int... book) {
 		if(book.length == 2){
-			return 15.2d;
+			return 8*2*(1-0.05);
 		} else if (book.length == 3){
 			return 8*3*(1-0.1);
 		}
