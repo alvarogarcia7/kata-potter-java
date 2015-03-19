3b45566a744a8b7995f62d23062db0a56e5b1a49
alvaro garcia <derecerca@gmail.com>
2015-03-19 20:54:47 +0100

    red: sell_0_books_at_zero_price

---
+++ src/test/java/PotterLibraryShould.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Created by alvaro on 19/03/15.
+ */
+public class PotterLibraryShould {
+
+	@Test
+	public void sell_0_books_at_zero_price () {
+
+		assertThat(price(), is(0));
+	}
+
+	private int price () {
+		return 1;
+	}
+}
