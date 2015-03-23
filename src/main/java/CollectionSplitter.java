10bc1a007fd14b21dc5391dc104f161b47431b28
alvaro garcia <derecerca@gmail.com>
2015-03-23 04:26:29 +0100

    green

--- src/main/java/CollectionSplitter.java
+++ src/main/java/CollectionSplitter.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
 * Created by alvaro on 23/03/15.
@@ -16,8 +18,48 @@ class CollectionSplitter {
 	}
 
 	private List<int[]> splitInCollections ( final int[] allBooks) {
-		final List<int[]> collections = new ArrayList<>();
-		collections.add(allBooks);
-		return collections;
+		final List<Set<Integer>> collections = new ArrayList<>();
+
+		Set<Integer> currentCollection = new HashSet<>();
+		collections.add(currentCollection);
+
+
+		for (int current : allBooks) {
+			if (currentCollection.contains(current)) {
+				boolean found = false;
+				for (Set<Integer> collection : collections) {
+					if(!collection.contains(current)){
+						currentCollection = collection;
+						found = true;
+					}
+				}
+				if(!found) {
+					currentCollection = new HashSet<>();
+					collections.add(currentCollection);
+				}
+
+			}
+			currentCollection.add(current);
+		}
+
+		return toIntArray(collections);
+	}
+
+	private List<int[]> toIntArray (final List<Set<Integer>> collections) {
+		final List<int[]> result = new ArrayList<>();
+
+		for (Set<Integer> collection : collections) {
+
+			final int[] currentResult = new int[collection.size()];
+			int i = 0;
+			for (Integer current : collection) {
+				currentResult[i] = current;
+				i++;
+			}
+
+			result.add(currentResult);
+		}
+
+		return result;
 	}
 }
